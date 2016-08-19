d49511e464e544e7c0de29e09f25db7feb47a501
ashley <msashleychen@gmail.com>
2016-08-18 21:59:56 -0400

    fixed username

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 # GWI-SquareCodeCamp
 Hello
 ogin/Register Feature
+=======
+# GWI-SquareCodeCamp
+Login/Register Feature
+testing using git, hi -melanie
+>>>>>>> 8083a276304aa91f1f0a325f996bc5a02a13de58
--- login/db/migrate/20160817144418_devise_create_users.rb
+++ login/db/migrate/20160817144418_devise_create_users.rb
@@ -3,7 +3,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :username,              null: false, default: ""
-      t.string :full_name,          null:false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
