29d9da2ea3a6377ca6eb85635732d56fd3c1642d
ashley <msashleychen@gmail.com>
2016-08-19 10:18:57 -0400

    Design finished

--- login/config/routes.rb
+++ login/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do	
+  get 'users/new'
+
 devise_for :users, :path_names => { :sign_up => "register" }
-root to: redirect('/sign_in')
+root to: redirect('/users/sign_in')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
