fefa2127fa0d2d302f2c2203b39b4ccda55532fb
Melanie <mendom16@uw.edu>
2016-08-19 11:21:16 -0400

    please work

--- login/config/routes.rb
+++ login/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do	
   get 'users/new'
-
-devise_for :users, :path_names => { :sign_up => "register" }
-root to: redirect('/users/sign_in')
+  get 'pages/profile'
+devise_for :users, :path_names => { :sign_up => "register" }, controllers: {
+        registrations: 'users/registrations'}
+#root to: redirect('/users/sign_in')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
