18bdadd0057e7fc138947592152f9c1619ffad91
“Angel” <aklyce@berkeley.edu>
2016-08-19 11:22:16 -0400

    bl

--- login/db/schema.rb
+++ login/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160817144418) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "", null: false
+    t.string   "full_name",              default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
